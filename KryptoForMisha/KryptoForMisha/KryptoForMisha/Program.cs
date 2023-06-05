3a48cc444e5dd8a2e69dab5000245dcc3e13df71
Ivan Polkanov <polkanovclassicpro@gmail.com>
2023-06-05 11:38:49 -0700

    Save changes

--- KryptoForMisha/KryptoForMisha/KryptoForMisha/Program.cs
+++ KryptoForMisha/KryptoForMisha/KryptoForMisha/Program.cs
@@ -1,8 +1,13 @@
+using Org.BouncyCastle.Asn1.Pkcs;
+using Org.BouncyCastle.Asn1;
 using Org.BouncyCastle.Crypto;
+using Org.BouncyCastle.Crypto.Encodings;
+using Org.BouncyCastle.Crypto.Engines;
 using Org.BouncyCastle.Crypto.Parameters;
 using Org.BouncyCastle.Security;
 using System.ComponentModel.Design;
 using System.Text;
+using System.Net.WebSockets;
 
 namespace KryptoForMisha
 {
@@ -36,13 +41,34 @@ namespace KryptoForMisha
             /* Make the key */
             //RsaKeyParameters key = MakeKey(privateModulusHexString, privateExponentHexString, true);
 
+            var a = new Org.BouncyCastle.Math.BigInteger("1234567890123456", 16);
+            var b = new Org.BouncyCastle.Math.BigInteger("1234567890123456", 16);
+            var c = new Org.BouncyCastle.Math.BigInteger("1234567890123456", 16);
+
+            var ccc = new Gost3410Parameters(a,b,c);
+
+            var x = new Org.BouncyCastle.Math.BigInteger("11", 2);
+            var kek = new Gost3410PrivateKeyParameters(x, ccc);
+
             /* Init alg */
             ISigner sig = SignerUtilities.GetSigner("GOST3411WITHECGOST3410-2012-256");
+            
+            //var key = new TestClass();
 
-            var key = new TestClass();
+            //const string keyPath = "C:\\Users\\polka\\Documents\\Newfolder\\zalupa.txt";
+
+            //using (var textReader = File.OpenText(keyPath))
+            //{
+            //    var pemReader = new Org.BouncyCastle.OpenSsl.PemReader(textReader);
+            //    var pemObj = pemReader.ReadPemObject();
+            //    var seq = (Asn1Sequence)Asn1Object.FromByteArray(pemObj.Content);
+            //    var keyInfo = PrivateKeyInfo.GetInstance(seq);
+            //    var akp = Org.BouncyCastle.Security.PrivateKeyFactory.CreateKey(keyInfo);
+            //}
 
             /* Populate key */
-            sig.Init(true,key);
+            //sig.Init(true,key);
+            sig.Init(true, kek);
 
             /* Get the bytes to be signed from the string */
             var bytes = Encoding.UTF8.GetBytes(data);
